fa960997399edc46ecf01bd67dc77624f1eacfc6
mirekSBCS <33669390+mirekSBCS@users.noreply.github.com>
2018-12-05 11:20:00 -0800

    Mirek stuff
    Trackball controls, buttons

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 camera.position.z = 4;
 
+//TrackballControls
+var controls = new THREE.TrackballControls(camera);
+
 // Create a renderer with Antialiasing
 var renderer = new THREE.WebGLRenderer({antialias:true});
 
@@ -26,18 +29,17 @@ document.body.appendChild( renderer.domElement );
 // ------------------------------------------------
 
 // Create a Cube Mesh with basic material
-var texture = new THREE.TextureLoader().load( "models/bethhead3d.jpg" );
+var texture1 = new THREE.TextureLoader().load( "models/bethhead3d.jpg" );
+var texture2 = new THREE.TextureLoader().load( "models/mirekHead3d.jpg")
 
-
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var materialTexture = new THREE.MeshBasicMaterial( { map: texture }); 
+var materialTexture1 = new THREE.MeshBasicMaterial( { map: texture1 });
+var materialTexture2 = new THREE.MeshBasicMaterial( { map: texture2} );
 var materialSolid = new THREE.MeshBasicMaterial( { color: "#433F81" } );
 
-var texture = new THREE.TextureLoader().load( "models/duckhat.mtl" );
-var materialDuck = new THREE.MeshBasicMaterial( { map: texture }); 
+var textureDuck = new THREE.TextureLoader().load( "models/duckhat.mtl" );
+var materialDuck = new THREE.MeshBasicMaterial( { map: textureDuck });
 //var cube = new THREE.Mesh( geometry, material );
-
-
+// var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 // Add cube to Scene
 // scene.add( cube );
 
@@ -45,57 +47,64 @@ var objects = {}
 
 var loader = new THREE.OBJLoader();
 
-// load a resource
-loader.load(
-	// resource URL
-	'models/bethhead3d.obj',
-	// called when resource is loaded
-	function ( object ) {
-		size = .01
-		object.scale.x = size
-		object.scale.y = size
-		object.scale.z = size
+function loadHead(head, materialTexture, landmarks){
 
-		object.children[0].material = materialTexture
-
-		objects["head"] = object
-		scene.add( object );
-	},
-	// called when loading is in progresses
-	function ( xhr ) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-	},
-	// called when loading has errors
-	function ( error ) {
-		console.log( 'An error happened' );
+	//clear Scene
+	while(scene.children.length > 0){
+		scene.remove(scene.children[0]);
 	}
-);
+	
+	// load a resource
+	loader.load(
+		// resource URL
+		head,
+		// called when resource is loaded
+		function ( object ) {
+			size = .01
+			object.scale.x = size
+			object.scale.y = size
+			object.scale.z = size
 
-loader.load(
-	// resource URL
-	'models/bethfaceLandmarks.obj',
-	// called when resource is loaded
-	function ( object ) {
-		size = .01
-		object.scale.x = size
-		object.scale.y = size
-		object.scale.z = size
+			object.children[0].material = materialTexture
 
-		object.children[0].material = materialSolid;
+			objects["head"] = object
+			scene.add( object );
+		},
+		// called when loading is in progresses
+		function ( xhr ) {
+			console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+		},
+		// called when loading has errors
+		function ( error ) {
+			console.log( 'An error happened' );
+		}
+	);
 
-		objects["faceLandmarks"] = object;
-		scene.add( object );
-	},
-	// called when loading is in progresses
-	function ( xhr ) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-	},
-	// called when loading has errors
-	function ( error ) {
-		console.log( 'An error happened' );
-	}
-);
+	loader.load(
+		// resource URL
+		landmarks,
+		// called when resource is loaded
+		function ( object ) {
+			size = .01
+			object.scale.x = size
+			object.scale.y = size
+			object.scale.z = size
 
+			object.children[0].material = materialSolid;
+
+			objects["faceLandmarks"] = object;
+			scene.add( object );
+		},
+		// called when loading is in progresses
+		function ( xhr ) {
+			console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+		},
+		// called when loading has errors
+		function ( error ) {
+			console.log( 'An error happened' );
+		}
+	);
+}
 // loader.load(
 // 	// resource URL
 // 	'models/duckhat.obj',
@@ -125,31 +134,31 @@ loader.load(
 // 	}
 // );
 
-var mtlLoader = new THREE.MTLLoader();
-mtlLoader.setPath('models/');
-mtlLoader.load('duckhat.mtl', function(materials) {
-  	materials.preload();
-  	var objLoader = new THREE.OBJLoader();
-  	objLoader.setMaterials(materials);
-  	objLoader.setPath('models');
-  	objLoader.load('duckhat.obj', function(object) {
-	    	size = 1.5
-			object.scale.x = size
-			object.scale.y = size
-			object.scale.z = size
-
-			object.position.y = -3
-	    	scene.add(object);
-	  	}, 
-	  	function ( xhr ) {
-			console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-		},
-		// called when loading has errors
-		function ( error ) {
-			console.log( 'An error happened' );
-		}
-	)
-});
+// var mtlLoader = new THREE.MTLLoader();
+// mtlLoader.setPath('models/');
+// mtlLoader.load('duckhat.mtl', function(materials) {
+//   	materials.preload();
+//   	var objLoader = new THREE.OBJLoader();
+//   	objLoader.setMaterials(materials);
+//   	objLoader.setPath('models');
+//   	objLoader.load('duckhat.obj', function(object) {
+// 	    	size = 1.5
+// 			object.scale.x = size
+// 			object.scale.y = size
+// 			object.scale.z = size
+//
+// 			object.position.y = -3
+// 	    	scene.add(object);
+// 	  	},
+// 	  	function ( xhr ) {
+// 			console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+// 		},
+// 		// called when loading has errors
+// 		function ( error ) {
+// 			console.log( 'An error happened' );
+// 		}
+// 	)
+// });
 
 // Render Loop
 var render = function () {
@@ -165,6 +174,7 @@ var render = function () {
 
 	// }
 
+	controls.update();
   // Render the scene
   renderer.render(scene, camera);
 };
