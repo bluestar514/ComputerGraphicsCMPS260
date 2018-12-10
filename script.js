d3f5af13024519d5a9dcd0dae5bc894911402997
Beth Oliver <bluestar514@gmail.com>
2018-12-10 13:48:07 -0800

    Revert "cleaned up refrences to variables that don't exist anymore"
    This reverts commit 5b116517fd4ac32be5c9426f4259c39ad8a17f51.

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ renderer.localClippingEnabled = true;
 var loader = new THREE.OBJLoader();
 
 function loadHead(head, textureName, landmarks){
-	localPlane.constant = 3
+	localPlane.constant = planeInitPos
 	// var localPlane = loadLandmark(landmarks);
 	var texture = new THREE.TextureLoader().load( textureName );
 	var materialTexture = new THREE.MeshPhongMaterial( { map: texture,
@@ -105,7 +105,8 @@ function loadHead(head, textureName, landmarks){
 }
 
 function loadHat(objectName, clippingPlaneLocation){
-
+	console.log(localPlane.constant)
+	console.log(headTop)
 	localPlane.constant = clippingPlaneLocation - headTop
 	loadAccessory(objectName, "hat")
 
